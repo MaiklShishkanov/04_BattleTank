0d28602b8cdc8f5ad89367f1b8c985fdcdfd601b
COMPUTER\oldpc <maiklrus1996@yandex.ru>
2021-11-19 12:03:09 +0300

    16_Setup Track feat ApplyForceAtLocation and Phys.Mat & Friction

---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * 
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+		
+public:
+	//устанавливаем значение для гусеницы между -1 и +1
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	//это мксимальная сила на гусеницу в Ньютанах
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 400000;//масса танка 40 тонн и ускорение в 1g
+};
